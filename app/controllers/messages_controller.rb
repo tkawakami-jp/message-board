7d65aba1b55feebba3a649d49459458079e21c3d
tkawakami <tkawakami@tkltd.jp>
2015-08-24 06:37:23 +0000

    edit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,7 @@
 class MessagesController < ApplicationController
+  
+  before_action :set_message, only: [:edit, :update]
+  
   def index
     @messages = Message.all
     @message = Message.new
@@ -15,9 +18,25 @@ class MessagesController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      redirect_to root_path , notice: 'メッセージを編集しました。'
+    else
+      render 'edit'
+    end
+  end
+  
   private
+  
   def message_params
     params.require(:message).permit(:name, :body)
+  end
+  
+  def set_message
+    @message = Message.find(params[:id])
   end
   
 end
