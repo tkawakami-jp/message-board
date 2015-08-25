93cd020765954150e343c174d5b48cfe52d5f9f0
tkawakami <tkawakami@tkltd.jp>
2015-08-25 01:47:45 +0000

    challenge2

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150824032822) do
+ActiveRecord::Schema.define(version: 20150825012744) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
